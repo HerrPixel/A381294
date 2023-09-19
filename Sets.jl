5bd1b9d63ab85f79db4ee0d44b5daaa3777cd6d7
Jonas <Me@jonasseiler.de>
2023-09-19 17:15:10 +0200

    add BruteForceSearch function

--- Sets.jl
+++ Sets.jl
@@ -1,3 +1,5 @@
+using Combinatorics
+
 function printSolution(solution::BitMatrix)
     sizes = size(solution)
     height = sizes[1]
@@ -22,3 +24,40 @@ function printSolution(solution::BitMatrix)
         end
     end
 end 
+
+function BruteForceSearch(n::Integer,k::Integer) 
+    solutions = Vector{BitMatrix}()
+    vectors = Vector{Vector{<:Integer}}()
+    for i in 0:2^k-1
+        push!(vectors,digits(i,base = 2, pad=k))
+    end
+
+    combinations = Vector{Vector{<:Integer}}()
+    for i in 0:length(vectors)^n-1
+        push!(combinations,digits(i,base=length(vectors),pad=n))
+    end
+
+    for x in combinations
+        z = Vector{Vector{<:Integer}}()
+        for i in x
+            push!(z,vectors[i+1])
+        end
+        y = vcat(transpose.(z)...)
+        correct = true
+
+        for i in 1:n
+            for j in i+1:n
+                if y[i,:]' * y[j,:] != j-i
+                    correct = false
+                end 
+            end
+        end
+
+        if correct
+            push!(solutions,BitArray(y))
+        end
+    end
+
+    return solutions
+end
+
