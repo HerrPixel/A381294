e671f0280ec2b8e63e0b33b8d7b35eba35b5ce9c
Jonas <Me@jonasseiler.de>
2023-09-25 21:50:49 +0200

    finished rewrite

--- src/Solution.jl
+++ src/Solution.jl
@@ -11,9 +11,14 @@ struct SetSolution
         matrix = BitMatrix(
             map(
                 x -> x > 0 ? 1 : 0,
-                reduce(hcat, a)'
+                reduce(hcat, rows)'
             ))
 
+        return new(matrix, size(matrix, 1), size(matrix, 2))
+    end
+
+    function SetSolution(rows::Vector{BitVector})
+        matrix = BitMatrix(reduce(hcat, rows)')
         return new(matrix, size(matrix, 1), size(matrix, 2))
     end
 end
@@ -33,18 +38,26 @@ function Base.getindex(s::SetSolution, row::Integer, column::Integer)
     throw(BoundsError(s.solutionmatrix, [row, column]))
 end
 
+function IsCorrect(s::SetSolution)
+    for i in 1:GetN(s)
+        for j in i:GetN(s)
+            if s.solutionmatrix[i, :]' * s.solutionmatrix[j, :] != j - i
+                return false
+            end
+        end
+    end
+    return true
+end
 
 function PrintSolution(s::SetSolution)
-    sizes = size(solution)
-
     # To make the output aligned, we calculate the maximum width a cell could need
     # The display of the chosen numbers in the sets takes up the number of digits as width
-    cellwidth = ceil(Int, log(10, s.GetK))
+    cellwidth = ceil(Int, log(10, GetK(s)))
 
     # The width of the first column is 1 + number of digits in the indices, the first one being for the 'A'
-    setnumberingwidth = 1 + ceil(Int, log(10, getN(s)))
+    setnumberingwidth = 1 + ceil(Int, log(10, GetN(s)))
 
-    for i in 1:getN(s)
+    for i in 1:GetN(s)
 
         println()
         print(rpad("A" * SubscriptNumber(i), setnumberingwidth))
