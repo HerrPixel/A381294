291955b78eb787459368632bedfc034acc82c5ee
Jonas Seiler <Me@jonasseiler.de>
2025-02-18 10:19:50 +0100

    add option to use custom solver and options

--- src/Solver.jl
+++ src/Solver.jl
@@ -1,8 +1,16 @@
 using JuMP
-using GLPK
+using HiGHS
 
-function solve(n::Integer)
-    model = Model(GLPK.Optimizer)
+function solve(n::Integer, solver, arguments::Vector{Tuple{Any,Any}})
+    model = Model(solver)
+
+    for (attribute, value) in arguments
+        set_attribute(model, attribute, value)
+    end
+
+    # TODO: add upper bound, warm start
+
+    set_string_names_on_creation(model, false)
 
     sets_containing_i, sets_containing_i_and_j = precompute(n)
 
@@ -23,6 +31,9 @@ function solve(n::Integer)
     return solution_summary(model)
 end
 
+solve(n::Integer) = solve(n, HiGHS.Optimizer)
+solve(n::Integer, solver) = solve(n, solver, Tuple{Any,Any}[])
+
 function precompute(n::Integer)
     sets_containing_i = falses(n, 2^n)
     sets_containing_i_and_j = falses(n, n, 2^n)
@@ -30,10 +41,6 @@ function precompute(n::Integer)
     for i in 1:n
         i_vector = falses(n)
         i_vector[i] = true
-
-        if y
-
-        end
 
         for j in (i+1):n
             j_vector = falses(n)
