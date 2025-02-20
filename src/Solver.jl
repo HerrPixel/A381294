d788bba34d088539dc65285b7869ca6c7e1a785d
Jonas Seiler <Me@jonasseiler.de>
2025-02-20 16:29:06 +0100

    add heuristic simplification to ILP

--- src/Solver.jl
+++ src/Solver.jl
@@ -1,7 +1,7 @@
 using JuMP
 using HiGHS
 
-function solve(n::Integer, solver, arguments::Vector{Tuple{Any,Any}})
+function solve(n::Integer, solver, arguments::Vector{Tuple{T,S}} where {T,S})
     model = Model(solver)
 
     for (attribute, value) in arguments
@@ -22,16 +22,28 @@ function solve(n::Integer, solver, arguments::Vector{Tuple{Any,Any}})
         end
     end
 
+    #= 
+    Elements can only be contained in at most 5 sets.
+    Heuristically, this produces at least one optimal solution for all n <= 25
+
+    for i in 1:2^n
+        if count_ones(i) > 5
+            @constraint(model, sets[i] == 0)
+        end
+    end
+    =#
+
     @constraint(model, sets[2^n] == 0)
 
     @objective(model, Min, sum(sets))
 
     optimize!(model)
 
-    return solution_summary(model)
+
+    return convertToSetSolution(n, round(Int, objective_value(model)), [round(Int, value(x)) for x in all_variables(model)])
 end
 
-solve(n::Integer) = solve(n, HiGHS.Optimizer)
+solve(n::Integer) = solve(n, HiGHS.Optimizer, [("presolve", "off"), ("mip_max_leaves", 100)])
 solve(n::Integer, solver) = solve(n, solver, Tuple{Any,Any}[])
 
 function precompute(n::Integer)
@@ -65,4 +77,32 @@ function precompute(n::Integer)
     end
 
     return sets_containing_i, sets_containing_i_and_j
+end
+
+function convertToSetSolution(n::Integer, k::Integer, sets)
+    curr_index = 1
+    solution = falses(n, k)
+
+    for (subset, number_of_elements) in enumerate(sets)
+        if number_of_elements == 0
+            continue
+        end
+
+        subsetvector = transpose(BitVector(digits(subset, base=2, pad=n)))
+
+        for setnumber in 1:n
+            setvector = falses(n)
+            setvector[setnumber] = true
+
+            if subsetvector * setvector == 1
+                for k in 0:number_of_elements-1
+                    solution[setnumber, curr_index+k] = true
+                end
+            end
+        end
+
+        curr_index += number_of_elements
+    end
+
+    return SetSolution(sortslices(solution, dims=2, rev=true))
 end
